60c9b52a6f40ad4842c56ef81c354c8f3fff8fbf
kaledmolina <kaledmoly@gmail.com>
2025-12-08 10:53:41 -0500

    edit de nombre perfil

---
+++ app/Filament/Pages/Auth/EditProfile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Pages\Auth\EditProfile as BaseEditProfile;
+
+class EditProfile extends BaseEditProfile
+{
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Nombre')
+                    ->required()
+                    ->maxLength(255),
+                TextInput::make('email')
+                    ->label('Correo Electrónico')
+                    ->email()
+                    ->required()
+                    ->maxLength(255)
+                    ->disabled(), // Disable email editing
+                // Password fields omitted
+            ]);
+    }
+}
