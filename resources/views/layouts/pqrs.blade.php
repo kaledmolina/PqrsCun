519adc37e7f46044682676afe379c2da83d4cb79
kaledmolina <kaledmoly@gmail.com>
2025-12-16 14:55:59 -0500

    fix: Update contact information for PQR line and email address.

--- resources/views/layouts/pqrs.blade.php
+++ resources/views/layouts/pqrs.blade.php
@@ -240,7 +240,7 @@
                                 <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path></svg>
                             </div>
                             <div>
-                                <p class="font-bold text-slate-900">Línea Nacional</p>
+                                <p class="font-bold text-slate-900">Línea de PQR</p>
                                 <p>3148042601</p>
                             </div>
                         </li>
@@ -250,7 +250,7 @@
                             </div>
                             <div>
                                 <p class="font-bold text-slate-900">Correo Electrónico</p>
-                                <p>servicioalcliente@intalnet.com</p>
+                                <p>pqr@intalnet.com</p>
                             </div>
                         </li>
                     </ul>
